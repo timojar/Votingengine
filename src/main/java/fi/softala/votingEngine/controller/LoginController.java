9006b8adece3daac9dc17d4577ee0e038fa0060b
Timo Jarmala <Timo Jarmala>
2016-10-25 18:35:04 +0300

    Email valmistelua

--- src/main/java/fi/softala/votingEngine/controller/LoginController.java
+++ src/main/java/fi/softala/votingEngine/controller/LoginController.java
@@ -1,18 +1,24 @@
 package fi.softala.votingEngine.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import fi.softala.votingEngine.emailservice.SpostiLahetys;
 
 
 @Controller
 @RequestMapping (value="/")
 public class LoginController {
  
+	@Autowired
+	private SpostiLahetys lahetys;
+	
 	@RequestMapping(value="/loginpage", method = RequestMethod.GET)
 	public String login(Model model) {
- 
+		lahetys.sendMail("timo.jarmala@myy.haaga-helia.fi", "subject", "body");
 		return "login";
  
 	}
