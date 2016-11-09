c387adf8254c9407c98cee75844faa94a5c956d7
Timo Jarmala <Timo Jarmala>
2016-11-10 01:40:09 +0200

    Aanioikeus

--- src/main/java/fi/softala/votingEngine/dao/aani/DaoAani.java
+++ src/main/java/fi/softala/votingEngine/dao/aani/DaoAani.java
@@ -2,12 +2,14 @@ package fi.softala.votingEngine.dao.aani;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import fi.softala.votingEngine.bean.Aani;
 
 
 public interface DaoAani {
 	
-	
+@PreAuthorize("hasAnyRole('ROLE_ADMIN, ROLE_USER')")	
 abstract void talletaAani(Aani aani);
 abstract void muutaoikeuksia(int opiskelijaId);
 public abstract List<Aani> listaaTulokset();
